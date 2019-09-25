1510a65451b01e79caae138292696b77b4692a67
TuanTrungVU <trung.lespace@gmail.com>
2019-09-25 18:28:42 +0200

    Update ObjectTypes

--- ExampleTools/ToolClasses/ObjectTypes.cs
+++ ExampleTools/ToolClasses/ObjectTypes.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace ToolClasses
 {
-    struct ObjectStruct
+
+    /* When a value-type instance is created, a single space in memory is allocated to store the value.
+     * Primitive types such as int, float, bool and char are also value types, and work in the same way.
+     * When the runtime deals with a value type, it's dealing directly with its underlying data and this can be very efficient, particularly with primitive types.
+     * 
+     * When a variable is set to another one, it is a COPY of the original variable.
+     * It means there are 2 locations in the memory and they are independents.
+     * When you change one of these variables, there is not effect to the other one.
+     */
+    public struct ObjectStruct
     {
         private string _name;
         private string _type;
@@ -20,7 +29,7 @@ namespace ToolClasses
             _type = type;
         }
 
-        public string NameStruct
+        public string NameObject
         {
             get { return _name; }
             set { _name = value; }
@@ -32,7 +41,15 @@ namespace ToolClasses
             set { _type = value; }
         }
     }
-    class ObjectClass
+
+
+    /* With reference types, however, an object is created in memory, and then handled through a separate reference—rather like a pointer.
+     * 
+     * When a variable is set to a reference, it will copy the original's reference to another.
+     * The result is they all point to the same object.
+     * 
+     */
+    public class ObjectClass
     {
         private string _name;
         private string _type;
---
+++ ExampleTools/UnitTestsTools/ObjectTypesTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToolClasses;
+
+namespace UnitTestsTools
+{
+    [TestFixture]
+    class ObjectTypesTests
+    {
+        [Test]
+        public void can_create_objects()
+        {
+            ObjectStruct objStruct = new ObjectStruct("Struct0");
+            Assert.That(objStruct.NameObject == "Struct0");
+            Assert.That(objStruct.Type == "Value");
+
+            ObjectClass objClass = new ObjectClass("Class0");
+            Assert.That(objStruct.NameObject == "Class0");
+            Assert.That(objStruct.Type == "Reference");
+        }
+
+        [Test]
+        public void test_changing_properties()
+        {
+            ObjectStruct objStruct = new ObjectStruct("Struct0");
+            ObjectStruct tmpVarStruct = objStruct;
+            tmpVarStruct.NameObject = "ChangedName";
+            tmpVarStruct.Type = "ChangedType";
+
+            Assert.That(objStruct.NameObject == "Struct0");
+            Assert.That(objStruct.Type == "Value");
+
+            Assert.That(tmpVarStruct.NameObject == "ChangedName");
+            Assert.That(tmpVarStruct.Type == "ChangedType");
+
+            ObjectClass objClass = new ObjectClass("Class0");
+            ObjectClass tmpVarClass = objClass;
+            tmpVarClass.NameObject = "ChangedName";
+            tmpVarClass.Type = "ChangedType";
+
+            Assert.That(objClass.NameObject == "ChangedName");
+            Assert.That(objClass.Type == "ChangedType");
+
+            Assert.That(tmpVarClass.NameObject == "ChangedName");
+            Assert.That(tmpVarClass.Type == "ChangedType");
+
+            ChangingValueTest0(tmpVarStruct, tmpVarClass);
+            Assert.That(tmpVarStruct.NameObject == "ChangedName");
+            Assert.That(tmpVarStruct.Type == "ChangedType");
+            Assert.That(tmpVarClass.NameObject == "ChangedTest0Name");
+            Assert.That(objClass.NameObject == "ChangedTest0Name");
+            Assert.That(objClass.Type == "Reference");
+
+            ChangingValueTest1(ref tmpVarStruct, ref tmpVarClass);
+            Assert.That(tmpVarStruct.NameObject == "ChangedTest1Name");
+            Assert.That(tmpVarClass == null);
+            Assert.That(objClass == null);
+
+        }
+
+        /* This is of particular interest when passing parameters to methods.
+         * In C#, parameters are (by default) passed by value, meaning that they are implicitly copied when passed to the method.
+         */
+        void ChangingValueTest0(ObjectStruct objS, ObjectClass objC)
+        {
+            /* For value-type parameters, this means physically copying the instance (in the same way of a struct was copied),
+             * while for reference-types it means copying a reference (in the same way of a reference was copied).
+             */
+            objS.NameObject = "ChangedTest0Name";
+            objC.NameObject = "ChangedTest0Name";
+            objC = null;
+        }
+
+        void ChangingValueTest1(ref ObjectStruct objS, ref ObjectClass objC)
+        {
+            objS.NameObject = "ChangedTest1Name";
+            objC.NameObject = "ChangedTest1Name";
+            objC = null;
+        }
+    }
+}
